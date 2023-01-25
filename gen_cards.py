933db0be82205f2302b43fcdcb1bacdf6e3fca8c
Helen Koike <helen@koikeco.de>
2023-01-25 10:04:49 -0300

    last version pratania

--- gen_cards.py
+++ gen_cards.py
@@ -6,10 +6,11 @@ import shutil
 import os
 from PIL import Image, ImageFont, ImageDraw
 import textwrap
+import utm
 
 # wrap and replace code like
 # card_img.text((660,92), nome_pop, color, font=font)
-def wrap_text(card_img, text, font, offset, width=60):
+def wrap_text(card_img, text, font, offset, width=33):
     lines = textwrap.wrap(text, width=width)
     x_text, y_text = offset
     for line in lines:
@@ -17,24 +18,39 @@ def wrap_text(card_img, text, font, offset, width=60):
         card_img.text((x_text, y_text), line, (0, 0, 0), font=font)
         y_text += height
 
+def convert_coord_to_utm(coord):
+    coord_y, coord_x = coord.split(", ") if coord else ("", "")
+    coord_y_utm, coord_x_utm, _, _ = utm.from_latlon(float(coord_y), float(coord_x)) if coord_y else ("", "", "", "")
+    utm_coord = f"Long {int(coord_y_utm)}; Lat {int(coord_x_utm)}"
+    return utm_coord
+
 def generate_card(template_file, pictures_folder, nome, nome_pop, nome_cien, coord, foto, output_folder):
     if not foto:
         return
 
     template = Image.open(template_file)
     card_img = ImageDraw.Draw(template)
-    font = ImageFont.truetype("Gidole-Regular.ttf", 40)
+    font = ImageFont.truetype("Gidole-Regular.ttf", 20)
+    font_header = ImageFont.truetype("Gidole-Regular.ttf", 10)
     foto_path = os.path.join(pictures_folder, foto)
     if (not os.path.isfile(foto_path)):
         print(f"ERROR: foto {foto_path} doesn't exist")
         return
 
-    foto_img = Image.open(foto_path).resize((898-181, 1238-282)) # side, end (898, 1238) - start
-    template.paste(foto_img, (181, 201))
+    right_margin = 27
+    foto_img = Image.open(foto_path).resize((330-right_margin, 445-50))
+    template.paste(foto_img, (right_margin, 50))
 
-    wrap_text(card_img, "Nome: " + nome_pop, font, (181,1200))
-    wrap_text(card_img, "Nome científico: " + nome_cien, font, (181,1311-20))
-    wrap_text(card_img, "Coordenadas: " + coord, font, (181,1423-40))
+    line_height = 46/2
+    inicial_line = 468-18
+
+    wrap_text(card_img, "Nome popular:", font_header, (right_margin, 3+inicial_line))
+    wrap_text(card_img, "    " + nome_pop, font, (right_margin, inicial_line + line_height))
+    wrap_text(card_img, "Nome científico:", font_header, (right_margin, 3+inicial_line + 2*line_height))
+    wrap_text(card_img, "    " + nome_cien, font, (right_margin, inicial_line + 3*line_height))
+    wrap_text(card_img, "Coordenadas:", font_header, (right_margin, 3+inicial_line + 4*line_height))
+    coord = convert_coord_to_utm(coord)
+    wrap_text(card_img, "    " + coord, font, (right_margin, inicial_line + 5*line_height))
 
     output = os.path.join(output_folder, nome + ".png")
     template.save(output)
@@ -50,7 +66,8 @@ def main(csv_file, pictures_folder, template="template.png", output_folder="resu
         i = 0
         for row in reader:
             i += 1
-            (uniq_id, parcela, nome_cien, familia, nome_pop, coord, altura, cap1, cap2, cap3, dap, foto1, foto2, foto3, foto4, foto5, foto6) = row
+            #(uniq_id, parcela, nome_cien, familia, nome_pop, coord, altura, cap1, cap2, cap3, dap, foto1, foto2, foto3, foto4, foto5, foto6) = row
+            (uniq_id, parcela, coord, nome_pop, nome_cien, altura, cap1, cap2, dap1, dap2, foto1, foto2, foto3, foto4, foto5, foto6) = row
             #print(uniq_id, parcela, coord, nome_pop, nome_cien, altura, cap, dap, foto1, foto2, foto3, foto4, foto5, foto6)
 
 
